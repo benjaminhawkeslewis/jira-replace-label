96475e979f36023c54c7d1391d1903eea503fcfa
Benjamin Hawkes-Lewis <benjamin.hawkes-lewis@imagini.net>
2013-06-24 15:49:42 +0100

    Add distutils setup.py install script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='jira-replace-label',
+    version='0.1',
+    scripts=['bin/jira-replace-label'],
+    requires=['jira (>0.13)']
+)
+      
+    
