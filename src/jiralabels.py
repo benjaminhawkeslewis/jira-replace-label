1aa8de3e21f9d7b9de70cfeef51bf539465bc6c3
Benjamin Hawkes-Lewis <benjamin.hawkes-lewis@imagini.net>
2013-06-25 14:07:44 +0100

    Add functions for adding or removing labels by query

--- src/jiralabels.py
+++ src/jiralabels.py
@@ -35,16 +35,38 @@ class JiraLabels(object):
             self._listeners[event_class] = []
         self._listeners[event_class].append(callback)
 
+    def add_label_by_query(self, j, jql, label):
+        issues = j.search_issues(jql)
+        for issue in issues:
+            old_labels = issue.fields.labels
+            new_labels = issue.fields.labels
+            if not label in issue.fields.labels:
+                new_labels.append(label)
+            self._label_issue(issue, new_labels)
+
+    def remove_label_by_query(self, j, jql, label):
+        issues = j.search_issues(jql)
+        for issue in issues:
+            old_labels = issue.fields.labels
+            new_labels = issue.fields.labels
+            if label in issue.fields.labels:
+                new_labels.append(label)
+            self._label_issue(issue, new_labels)
+
     def replace_label(self, j, old_label, new_label):
         issues = j.search_issues('labels in ("%s")' % old_label)
         for issue in issues:
-            old_labels = issue.fields.labels
-            labels = [new_label if label == old_label else label for label in issue.fields.labels]
-            try:
-                self._update_listeners(LabelEvent(issue, old_labels))
-            except jira.exceptions.JIRAError as e:
-                self._update_listeners(ErrorEvent(issue, e))
-        
+            new_labels = [new_label if label == old_label else label for label in issue.fields.labels]
+            self._label_issue(issue, new_labels)
+
+    def _label_issue(self, issue, new_labels):
+        old_labels = issue.fields.labels
+        try:
+            issue.update(labels=new_labels)
+            self._update_listeners(LabelEvent(issue, old_labels))
+        except jira.exceptions.JIRAError as e:
+            self._update_listeners(ErrorEvent(issue, e))
+         
     def _update_listeners(self, event):
         if event.__class__.__name__ in self._listeners:
             for callback in self._listeners[event.__class__.__name__]:
