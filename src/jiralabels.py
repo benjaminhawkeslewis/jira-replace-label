5de66e95c448abcac13fdc70c988eaab3f411fd7
Benjamin Hawkes-Lewis <benjamin.hawkes-lewis@imagini.net>
2013-06-28 11:45:47 +0100

    Replace event listeners with generators
    Rather than provide event handlers to capture issue updates and
    exceptions, and handle appropriately in the client, make functions
    to update labels into generators that can be looped, returning
    information about the last attempt to update an issue.

--- src/jiralabels.py
+++ src/jiralabels.py
@@ -2,38 +2,8 @@
 
 import jira.client
 import jira.exceptions
-import requests.exceptions
-import sys
-import signal
-import argparse
-import getpass
-
-class Event(object):
-    
-    def __init__(self, issue):
-        self.issue = issue
-
-class ErrorEvent(Event):
-    
-    def __init__(self, issue, exception):
-        self.exception = exception
-        super(ErrorEvent, self).__init__(issue)
-
-class LabelEvent(Event):
-    
-    def __init__(self, issue, old_labels):
-        self.old_labels = old_labels
-        super(LabelEvent, self).__init__(issue)
 
 class JiraLabels(object):
-
-    def __init__(self):
-        self._listeners = {}
-
-    def add_listener(self, event_class, callback):
-        if not event_class in self._listeners:
-            self._listeners[event_class] = []
-        self._listeners[event_class].append(callback)
 
     def add_label_by_query(self, j, jql, label):
         issues = j.search_issues(jql)
@@ -42,30 +12,25 @@ class JiraLabels(object):
             new_labels = issue.fields.labels
             if not label in issue.fields.labels:
                 new_labels.append(label)
-            self._label_issue(issue, new_labels)
+            yield self._label_issue(issue, new_labels)
 
     def remove_label_by_query(self, j, jql, label_to_remove):
         issues = j.search_issues(jql)
         for issue in issues:
             old_labels = issue.fields.labels
             new_labels = [label for label in issue.fields.labels if label != label_to_remove]
-            self._label_issue(issue, new_labels)
+            yield self._label_issue(issue, new_labels)
 
     def replace_label(self, j, old_label, new_label):
         issues = j.search_issues('labels in ("%s")' % old_label)
         for issue in issues:
             new_labels = [new_label if label == old_label else label for label in issue.fields.labels]
-            self._label_issue(issue, new_labels)
-
+            yield self._label_issue(issue, new_labels)
+ 
     def _label_issue(self, issue, new_labels):
-        old_labels = issue.fields.labels
+        original_labels = issue.fields.labels
         try:
             issue.update(labels=new_labels)
-            self._update_listeners(LabelEvent(issue, old_labels))
+            return (issue, original_labels, None)
         except jira.exceptions.JIRAError as e:
-            self._update_listeners(ErrorEvent(issue, e))
-         
-    def _update_listeners(self, event):
-        if event.__class__.__name__ in self._listeners:
-            for callback in self._listeners[event.__class__.__name__]:
-                callback(event) 
+            return (issue, original_labels, e)
--- test/test_jiralabels.py
+++ test/test_jiralabels.py
@@ -13,7 +13,9 @@ class TestJiraLabels(unittest.TestCase):
         jql = 'assignee = john.doe'
         label = 'somelabel'
         j.search_issues = Mock(return_value = [])
-        self.jiralabels.add_label_by_query(j, jql, label)
+
+        list(self.jiralabels.add_label_by_query(j, jql, label))
+
         j.search_issues.assert_called_once_with(jql)
 
     def test_add_label_by_query_with_results(self):
@@ -25,7 +27,9 @@ class TestJiraLabels(unittest.TestCase):
         issue.fields.labels = ['alpha', 'beta', 'gamma']
         issue.update = Mock() 
         j.search_issues = Mock(return_value = [issue])
-        self.jiralabels.add_label_by_query(j, jql, label)
+
+        self.jiralabels.add_label_by_query(j, jql, label).next()
+
         j.search_issues.assert_called_once_with(jql)
         issue.update.assert_called_once_with(labels=['alpha', 'beta', 'gamma', 'somelabel'])
 
@@ -38,7 +42,9 @@ class TestJiraLabels(unittest.TestCase):
         issue.fields.labels = ['alpha', label_to_remove, 'gamma']
         issue.update = Mock() 
         j.search_issues = Mock(return_value = [issue])
-        self.jiralabels.remove_label_by_query(j, jql, label_to_remove)
+
+        self.jiralabels.remove_label_by_query(j, jql, label_to_remove).next()
+
         j.search_issues.assert_called_once_with(jql)
         issue.update.assert_called_once_with(labels=['alpha', 'gamma'])
 
@@ -51,8 +57,8 @@ class TestJiraLabels(unittest.TestCase):
         issue.fields.labels = ['alpha', old_label, 'gamma']
         issue.update = Mock() 
         j.search_issues = Mock(return_value = [issue])
-        self.jiralabels.replace_label(j, old_label, new_label)
+
+        self.jiralabels.replace_label(j, old_label, new_label).next()
+            
         j.search_issues.assert_called_once_with('labels in ("old")')
         issue.update.assert_called_once_with(labels=['alpha', new_label, 'gamma'])
-
-
